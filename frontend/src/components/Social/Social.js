051e4a54e5b6b27008cfd742d641fc985aec109d
Sebastian Carrera <sebascarreram@hotmail.com>
2020-10-16 09:25:48 -0500

    Modified all alt and added an link for linkedin with icon

--- frontend/src/components/Social/Social.js
+++ frontend/src/components/Social/Social.js
@@ -8,21 +8,26 @@ import LogoGithub from "./../../assets/Icons/SVG/github-green.svg";
 import LogoInstagram from "./../../assets/Icons/SVG/instagram.svg";
 // import LogoMail from "./../../assets/Icons/mail-outline.svg";
 import LogoMail from "./../../assets/Icons/SVG/mail-outline.svg";
+import LogoLinkedin from "./../../assets/Icons/SVG/linkedin.svg";
 
 const Social = () => {
   return (
     <div className={style.Social}>
       <ul>
         <SocialItem NoTo href="https://www.instagram.com/sebascarreram/">
-          <img className={style.Logo} alt="logo icon" src={LogoInstagram} />
+          <img className={style.Logo} alt="logo instagram" src={LogoInstagram} />
         </SocialItem>
 
         <SocialItem NoTo href="https://github.com/sebascarreram/">
-          <img alt="logo icon" src={LogoGithub} />
+          <img alt="logo github" src={LogoGithub} />
+        </SocialItem>
+
+        <SocialItem NoTo href="https://www.linkedin.com/in/sebastiancarrera/">
+          <img alt="logo linkedin" src={LogoLinkedin} />
         </SocialItem>
 
         <SocialItem toLink="/contact">
-          <img alt="logo icon" src={LogoMail} />
+          <img alt="logo mail" src={LogoMail} />
         </SocialItem>
       </ul>
     </div>
